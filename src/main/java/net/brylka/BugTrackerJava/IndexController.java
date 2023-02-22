cffcfba3640dd39df7d77861b95e0b69fa03e065
brylka <brylka@jelenia.com>
2023-02-23 00:38:42 +0100

    Dodanie szablonu widoku i menu.

--- src/main/java/net/brylka/BugTrackerJava/IndexController.java
+++ src/main/java/net/brylka/BugTrackerJava/IndexController.java
@@ -2,13 +2,19 @@ package net.brylka.BugTrackerJava;
 
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/hello";
+    }
+
     @RequestMapping("/hello")
-    @Secured("ROLE_TEST")
+    @Secured("ROLE_ADMIN")
     public String hello() {
         return "test/test";
     }
